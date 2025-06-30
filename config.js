6012a7112e677b5a2f5f27f304286bfbe2cf73b3
Devin Peevy <devin.peevy@outlook.com>
2025-06-29 17:12:51 -0700

    Add comment instructions for the file.

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
+/*
+ * Use this file to configure the address of the player account/saving API host.
+ * The provided host `https://www.api.goodbadchad.bigdevdog.com` does not permit CORS
+ * requests from deployments of this game **other than** the one at 
+ * `https://www.goodbadchad.bigdevdog.com`.
+ */
+
 const config = {
   apiHost: 'https://www.api.goodbadchad.bigdevdog.com'
 };
